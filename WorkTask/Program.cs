afa7fd18b1d62cfaac5035be80b04ac34933f66a
motronovsky <Motronovskyi@smarttender.biz>
2023-04-25 22:10:20 +0300

    Line adder
    - Added comand line interface.

--- WorkTask/Program.cs
+++ WorkTask/Program.cs
@@ -1,7 +1,34 @@
 ﻿using LineSumator;
 
-var fileStrings = File.OpenText("D:\\Test.txt");
+string path;
 
-var result = LineSum.Calculate(fileStrings);
-Console.WriteLine(result.LineWithMaxSum);
-Console.WriteLine(string.Join(", ", result.LinesWithErrors));
+if (args.Length > 0)
+{
+    path = args[0];
+    Console.WriteLine("Path loaded from args: {0}", path);
+}
+else
+{
+    Console.Write("Enter path to file: ");
+    path = Console.ReadLine() ?? throw new InvalidOperationException();
+}
+
+if (!File.Exists(path))
+{
+    Console.WriteLine("File does not exist.");
+
+    return;
+}
+
+if (Path.GetExtension(path) != ".txt")
+{
+    Console.WriteLine("Path must have \"txt\" format.");
+
+    return;
+}
+
+using var streamReader = new StreamReader(path);
+var result = LineSum.Calculate(streamReader);
+
+Console.WriteLine("Line number with maximum sum: " + result.LineWithMaxSum ?? "Line with maximum sum does not found.");
+Console.WriteLine("Lines with error: {0}", string.Join(", ", result.LinesWithErrors));
